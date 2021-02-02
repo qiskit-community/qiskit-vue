7ef2bf7a33591b3c51768f19cf27d8ed6ee626b4
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-02 13:41:39 +0100

    chore: start new approach with vue/cli

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as BasicLink } from './BasicLink.vue';
-export { default as qiskitUiComponentsSample } from './qiskit-ui-components-sample.vue';
-export { default as UIShellHeader } from './UIShellHeader.vue';
