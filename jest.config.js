3c1f822560f59ac545d574339897e80979f6e2c7
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-02 13:57:12 +0100

    chore: cleanup directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  transform: {
-    "^.+\\.vue$": "vue-jest"
-  }
+  testMatch: ["**/components/**/*.spec.[jt]s?(x)"]
 };
