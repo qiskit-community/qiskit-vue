c396ceae93bb48b00d1066bbc1907e9dff8c33aa
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-06-28 16:29:30 +0200

    chore: update export syntax

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
-export { default as QBasicLink } from "./QBasicLink/index.vue";
-export { default as QLogo } from "./QLogo/index.vue";
+import QBasicLink from "./QBasicLink/index.vue";
+import QLogo from "./QLogo/index.vue";
+
+export { QBasicLink, QLogo };
