d11215f2df70d0f42af10317dde37323b118b63c
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-02 12:54:07 +0200

    feat(banner): test slot content rendering

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  collectCoverage: true,
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
