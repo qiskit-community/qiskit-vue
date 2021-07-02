199fe1378d018fe47af56ab0ca3126eaf14f74ba
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-02 12:55:47 +0200

    feat: create `Banner` component (#8)
    * feat(banner): create basic component
    
    * feat(banner): create stories with short and long content
    
    * feat(banner): use Carbon colors
    
    * feat(banner): use Carbon typography
    
    * feat(banner): use Carbon spacing
    
    * feat(banner): test slot content rendering

---
+++ src/components/QiskitBanner/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="qiskit-banner"><slot /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+@import "@carbon/colors/scss/colors";
+@import "@carbon/layout/scss/layout";
+@import "@carbon/type/scss/type";
+
+.qiskit-banner {
+  @include carbon--type-style("body-long-01");
+
+  background: $carbon--purple-80;
+  color: $white-0;
+  font-family: carbon--font-family("sans");
+  padding: $spacing-04 $spacing-06;
+}
+</style>
