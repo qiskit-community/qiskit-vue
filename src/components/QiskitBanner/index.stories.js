a4e1d509fec96fd214b2121a59a16e8e9b54f0fa
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-02 12:01:57 +0200

    feat(banner): create stories with short and long content

--- src/components/QiskitBanner/index.stories.js
+++ src/components/QiskitBanner/index.stories.js
@@ -11,8 +11,16 @@ const Template = (args) => ({
   setup() {
     return { args };
   },
-  template: '<qiskit-banner v-bind="args">Lorem ipsum</qiskit-banner>',
+  template: `<qiskit-banner v-bind="args">${args.default}</qiskit-banner>`,
 });
 
-export const Standard = Template.bind({});
-Standard.args = {};
+export const ShortContent = Template.bind({});
+ShortContent.args = {
+  default: "Lorem ipsum dolor sit amet",
+};
+
+export const LongContent = Template.bind({});
+LongContent.args = {
+  default:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+};
