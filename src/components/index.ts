81516a55dce20406637b047de7b276b6d70aa739
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-26 16:17:51 +0200

    feat: create `MegaMenuDropdown` component (#17)
    * feat(mega-menu-dropdown): create component
    
    * chore: update component exports
    
    * fix(mega-menu-dropdown): TS fix
    
    * feat(mega-menu-dropdown): "input field classes" prop
    
    * chore: remove unused tracking in component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as QBasicLink } from "./QBasicLink/index.vue";
+export { default as BasicLink } from "./BasicLink/index.vue";
+export { default as MegaMenuDropdown } from "./MegaMenuDropdown/index.vue";
 export { default as QLogo } from "./QLogo/index.vue";
 export { default as QiskitBanner } from "./QiskitBanner/index.vue";
--- src/links.ts
+++
@@ -1,3 +0,0 @@
-export interface SegmentData {
-  action: string;
-}
