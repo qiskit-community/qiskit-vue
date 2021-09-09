5b56c4271f905d6338517de5f79df348bc2f39bc
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-09-09 11:16:57 +0200

    feat: dark mode variables and logic (#18)
    * feat(dark-mode): global CSS variables
    
    * feat(dark-mode): get theme preference
    
    * feat(dark-mode): only logic

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as BasicLink } from "./BasicLink/index.vue";
 export { default as MegaMenuDropdown } from "./MegaMenuDropdown/index.vue";
 export { default as QLogo } from "./QLogo/index.vue";
 export { default as QiskitBanner } from "./QiskitBanner/index.vue";
+export { default as ThemeToggle } from "./ThemeToggle/index.vue";
