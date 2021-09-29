8cf568962ce19d577c0250b10a1a313cf9eb8c13
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-09-29 15:17:24 +0200

    ci: reorder changelog plugin

--- .releaserc.js
+++ .releaserc.js
@@ -10,14 +10,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
     [
       "@semantic-release/git",
       {
