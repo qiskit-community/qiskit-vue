8b17b0b37f2e2528602c9400055402e3b8bdb27a
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-09-29 15:10:53 +0200

    feat: remove QLogo (#24)
    BREAKING CHANGE: The logo component has been removed.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as BasicLink } from "./BasicLink/index.vue";
 export { default as MegaMenuDropdown } from "./MegaMenuDropdown/index.vue";
-export { default as QLogo } from "./QLogo/index.vue";
 export { default as QiskitBanner } from "./QiskitBanner/index.vue";
 export { default as ThemeToggle } from "./ThemeToggle/index.vue";
